8f060ac1b1ea0eca99a15c977a676dc51a928b3f
amito <ofirgaming123@gmail.com>
2025-11-24 13:58:13 +0200

    start of second day

---
+++ app/src/main/java/com/example/casino_israel/roulette.java
@@ -0,0 +1,10 @@
+package com.example.casino_israel;
+
+import android.content.Context;
+import android.view.View;
+
+public class roulette extends View {
+    public roulette(Context context) {
+        super(context);
+    }
+}
